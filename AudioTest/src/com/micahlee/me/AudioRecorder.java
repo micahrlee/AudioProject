fa3af9a3943c8b3c87be32380bf9ab1e1e29cb02
micahrlee <micahrlee@gmail.com>
2014-11-20 15:06:09 -0800

    Saved audio files, with automatic appending

--- AudioTest/src/com/micahlee/me/AudioRecorder.java
+++ AudioTest/src/com/micahlee/me/AudioRecorder.java
@@ -1,32 +1,49 @@
 package com.micahlee.me;
 
+import java.awt.Desktop;
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
-
+import javax.swing.JOptionPane;
 
 public class AudioRecorder extends Thread{
-	public static final AudioFormat format = new AudioFormat(8000.0f, 8, 1, true, true);
+	public static final AudioFormat format = new AudioFormat(8000.0f, 8, 1, true, false);
+	public AudioFileFormat.Type type;
+	public static final int bufSize = 4096;
 	private TargetDataLine dataLine;
-//	private SourceDataLine playBackLine;
 	private byte[] data;
+	private final String path;
+	private final boolean saveFile;
 	private boolean stop = true;
 	
-	public AudioRecorder(){
+	public AudioRecorder(final String path, final AudioFileFormat.Type type, final boolean saveFile){
 		initDataLine();
-//		initPlayBack();
 		stop = true;
+		if(type != null){
+			this.type = type;
+		}
+		else{
+			this.type = AudioFileFormat.Type.WAVE;
+		}
+		this.path = path;
+		this.saveFile = saveFile;
 	}
 	
 	private void initDataLine(){
-		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+		DataLine.Info info = new DataLine.Info(TargetDataLine.class, format, bufSize);
 		if(!AudioSystem.isLineSupported(info)){
-			System.out.println("Please use a microphone.");
+			JOptionPane.showMessageDialog(null, "Could not find microphone.\n", "Error", JOptionPane.ERROR_MESSAGE);
 			System.exit(0);
 		}
 		try{
@@ -34,32 +51,25 @@ public class AudioRecorder extends Thread{
 			dataLine.open(format);
 		}
 		catch(LineUnavailableException e){
-			System.out.println("Could not obtain audio line.");
+			JOptionPane.showMessageDialog(null, "Could not obtain recording line.", "Error", JOptionPane.ERROR_MESSAGE);
 		}
 	}
 	
-//	private void initPlayBack(){
-//		DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
-//		try {
-//			playBackLine = (SourceDataLine) AudioSystem.getLine(info);
-//			playBackLine.open();
-//		} catch (LineUnavailableException e) {
-//			System.out.println("Could not obtain audio line.");
-//		}
-//	}
-	
 	public void listen(){
+		System.out.println("Recording.");
 		stop = false;
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		data = new byte[4096];
+		data = new byte[bufSize];
+		int bytesRead;
 		//This is where we begin our audio capture
 		dataLine.start();
 		//This is where we store our data
-		int bytesRead;
 		while(!stop){
-			System.out.println("Recordingggggg");
 			bytesRead = dataLine.read(data, 0, data.length);
 			out.write(data, 0, bytesRead);
+		}
+		if(saveFile){
+			saveFile();
 		}
 	}
 	
@@ -69,19 +79,35 @@ public class AudioRecorder extends Thread{
 		dataLine.close();
 	}
 	
-	public void playBack(){
+	private void saveFile(){
+		InputStream audioData = new ByteArrayInputStream(data);
+		AudioInputStream ais = new AudioInputStream(audioData, format, (long)data.length);
+		try {
+			AudioSystem.write(ais, type, new File(path));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		if(JOptionPane.YES_OPTION == JOptionPane.showConfirmDialog(null, "File saved successfully!\n Open directory?", "Open Directory?", JOptionPane.YES_NO_OPTION)){
+			try {
+				Desktop.getDesktop().open(new File(System.getProperty("user.dir") + File.separator));
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+	
+	private void playBack(){
 		DataLine.Info info = new DataLine.Info(Clip.class, format);
 		try {
 			Clip clip = (Clip)AudioSystem.getLine(info);
 			clip.open(format, data, 0, data.length);
 			clip.start();
 		} catch (LineUnavailableException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public void filterOrWhatever(){
+	private void filterOrWhatever(){
 		
 	}
 
--- AudioTest/src/com/micahlee/me/AudioRecordingWindow.java
+++ AudioTest/src/com/micahlee/me/AudioRecordingWindow.java
@@ -4,6 +4,13 @@ import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.io.File;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
+import javax.sound.sampled.AudioFileFormat;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -11,19 +18,21 @@ import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JProgressBar;
-import javax.swing.JLabel;
-import javax.swing.JSpinner;
-import javax.swing.SpinnerNumberModel;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.JCheckBox;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerListModel;
 
 public class AudioRecordingWindow extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JButton record;
 	private JButton stopRecord;
+	private JSpinner fileExtenstion;
+	private JCheckBox saveFile;
 	private AudioRecorder ar;
+	private static final String fileName = "audio";
 
 	public AudioRecordingWindow() {
 		try {
@@ -39,7 +48,7 @@ public class AudioRecordingWindow extends JFrame {
 		}
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 213, 62);
+		setBounds(100, 100, 213, 103);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -48,21 +57,37 @@ public class AudioRecordingWindow extends JFrame {
 		
 		stopRecord = new JButton("Stop Recording");
 		stopRecord.setEnabled(false);
+		setLocationRelativeTo(null);
+		
+		saveFile = new JCheckBox("Save File");
+		saveFile.setSelected(true);
+		
+		fileExtenstion = new JSpinner();
+		fileExtenstion.setModel(new SpinnerListModel(new String[] {"WAV", "AIFF", "AIFC", "AU", "SND"}));
+		
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addComponent(record, GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
+						.addComponent(fileExtenstion, GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)
+						.addComponent(record, GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE))
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(stopRecord))
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(saveFile)
+						.addComponent(stopRecord)))
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.TRAILING)
-				.addGroup(Alignment.LEADING, gl_contentPane.createSequentialGroup()
+				.addGroup(gl_contentPane.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+						.addComponent(saveFile)
+						.addComponent(fileExtenstion, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.RELATED, 9, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(record)
-						.addComponent(stopRecord))
-					.addContainerGap(57, Short.MAX_VALUE))
+						.addComponent(stopRecord)))
 		);
 		contentPane.setLayout(gl_contentPane);
 		activateListeners();
@@ -72,7 +97,26 @@ public class AudioRecordingWindow extends JFrame {
 		record.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				ar = new AudioRecorder();
+				AudioFileFormat.Type type = null;
+				switch((String)fileExtenstion.getValue()){
+					case "WAV":
+						type = AudioFileFormat.Type.WAVE;
+						break;
+					case "AIFF":
+						type = AudioFileFormat.Type.AIFF;
+						break;
+					case "AIFC":
+						type = AudioFileFormat.Type.AIFC;
+						break;
+					case "AU":
+						type = AudioFileFormat.Type.AU;
+						break;
+					case "SND":
+						type = AudioFileFormat.Type.SND;
+						break;
+						
+				}
+				ar = new AudioRecorder(getPath() + getFileName(), type, saveFile.isSelected());
 				record.setEnabled(false);
 				stopRecord.setEnabled(true);
 				ar.start();
@@ -87,6 +131,33 @@ public class AudioRecordingWindow extends JFrame {
 				ar.stopRecord();
 			}
 		});
+	}
+	
+	private String getFileName(){
+		File testFile = new File(getPath() + File.separator);
+		ArrayList<File> list = new ArrayList<File>(Arrays.asList(testFile.listFiles()));
+		int append = 0;
+		if(!contains(list, Pattern.compile("audio\\..*"))){
+			return new String(fileName + "." + ((String) fileExtenstion.getValue()).toLowerCase());
+		}
+		while(contains(list, Pattern.compile("audio" + append + "\\..*"))){
+			++append;
+		}
+		
+		return new String(fileName + append + "." +((String) fileExtenstion.getValue()).toLowerCase());
+	}
+	
+	private boolean contains(ArrayList<File> al, Pattern p){
+		for(File f : al){
+			if(p.matcher(f.getName()).matches()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	private String getPath(){
+		return System.getProperty("user.dir") + File.separator;
 	}
 	
 	public static void main(String[] args) {
